62c3de535014164876b75b061d754c276623fd57
sushantpal005-pixel <sushantpal005@gmail.com>
2026-07-12 00:27:12 +0530

    square root using binary search

--- src/Binary_Search/lec_40/Main.java
+++ src/Binary_Search/lec_40/Main.java
@@ -5,7 +5,7 @@ public class Main {
     static int mySqrt(int x) {
         int s = 1;
         int e = x;
-        int ans = -1;
+        double ans = -1;
 
         if(x == 0){
             return 0;
@@ -28,10 +28,32 @@ public class Main {
                 s = mid + 1;
             }
         }
-        return ans;
+        double factor = 1;
+        int totalPrecision = 3;
+        for (int round = 1; round <= totalPrecision; round++) {
+            factor = factor / 10;
+            for (int i = 1; i <= 10 ; i++) {
+                double newAns = ans + factor;
+                  //equality code is ignored as of now, but make sure that you keep this in ur final code
+//                if (newAns * newAns == x) {
+//                    return newAns;
+//                }
+                if(newAns * newAns < x){
+                    ans = newAns;
+                }
+                else{
+                    break;
+                }
+            }
+            System.out.println("is round ka ans is: " + Math.round(ans * 1000.0) / 1000.0);
+        }
+        System.out.println("final round ka ans is: " + Math.round(ans * 1000.0) / 1000.0);
+
+        //returning dummy value
+        return -1;
     }
 
     static void main() {
-        System.out.println(mySqrt(49));
+        System.out.println(mySqrt(56));
     }
 }
