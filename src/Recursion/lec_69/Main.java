5694c82761e08603778dc1ab14525af9f806dc4c
sushantpal005-pixel <sushantpal005@gmail.com>
2026-09-23 23:10:31 +0530

    Combination Sum-1, 2, 3

--- src/Recursion/lec_69/Main.java
+++ src/Recursion/lec_69/Main.java
@@ -1,9 +1,88 @@
 package Recursion.lec_69;
 import java.util.*;
 public class Main {
-    static void solve(int[] candidates, int target, int index, List<List<Integer>> ans, List<Integer> output){
+    //combination sum - 1
+//    static void solve(int[] candidates, int target, int index, List<List<Integer>> ans, List<Integer> output){
+//        //base case
+//        if(target == 0) {
+//            //target = 0 ka mtlb output wali list me ek ans ban chuka h,
+//            //then store this ans into ans wali list
+//            ans.add(new ArrayList(output));
+//            return;
+//        }
+//        if(index >= candidates.length){
+//            return;
+//        }
+//        if(target < 0){
+//            return;
+//        }
+//        //ek case hum solve krenge baki recursion sambhal lega
+//        //include exclude
+//        //include
+//        output.add(candidates[index]);
+//        solve(candidates, target - candidates[index], index, ans, output);
+//        //backtracking wala step
+//        output.remove(output.size()-1);
+//
+//        //exclude
+//        solve(candidates, target, index + 1, ans, output);
+//
+//    }
+//    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+//        List<List<Integer>> ans = new ArrayList<>();
+//        List<Integer> output = new ArrayList<>();
+//        int index = 0;
+//        solve(candidates, target, index, ans, output);
+//        return ans;
+//    }
+
+    //combination sum - 2
+//    static void solve(int[] candidates, int target, int index, List<List<Integer>> ans, List<Integer> output){
+//        //base case
+//        if(target == 0) {
+//            //target = 0 ka mtlb output wali list me ek ans ban chuka h,
+//            //then store this ans into ans wali list
+//            ans.add(new ArrayList(output));
+//            return;
+//        }
+//        if(index >= candidates.length){
+//            return;
+//        }
+//        if(target < 0){
+//            return;
+//        }
+//        //ek case hum solve krenge baki recursion sambhal lega
+//        //include exclude
+//        //include
+//        output.add(candidates[index]);
+//        solve(candidates, target - candidates[index], index+1, ans, output);
+//        //backtracking wala step
+//        output.remove(output.size()-1);
+//
+//        //to generate unique combination
+//        while(index + 1 < candidates.length && candidates[index] == candidates[index+1]){
+//            index++;
+//        }
+//        //exclude
+//        solve(candidates, target, index + 1, ans, output);
+//
+//    }
+//    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+//        Arrays.sort(candidates);
+//        List<List<Integer>> ans = new ArrayList<>();
+//        List<Integer> output = new ArrayList<>();
+//        int index = 0;
+//        solve(candidates, target, index, ans, output);
+//        return ans;
+//    }
+
+    // combination sum - 3
+    static void solve(int[] candidates, int target, int index, List<List<Integer>> ans, List<Integer> output, int count, int k){
         //base case
-        if(target == 0) {
+        if(count > k){
+            return;
+        }
+        if(count == k && target == 0) {
             //target = 0 ka mtlb output wali list me ek ans ban chuka h,
             //then store this ans into ans wali list
             ans.add(new ArrayList(output));
@@ -19,19 +98,26 @@ public class Main {
         //include exclude
         //include
         output.add(candidates[index]);
-        solve(candidates, target - candidates[index], index, ans, output);
+        solve(candidates, target - candidates[index], index+1, ans, output, count + 1, k);
         //backtracking wala step
         output.remove(output.size()-1);
 
+        //to generate unique combination
+        while(index + 1 < candidates.length && candidates[index] == candidates[index+1]){
+            index++;
+        }
         //exclude
-        solve(candidates, target, index + 1, ans, output);
+        solve(candidates, target, index + 1, ans, output, count, k);
 
     }
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    public List<List<Integer>> combinationSum3(int k, int n) {
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> output = new ArrayList<>();
         int index = 0;
-        solve(candidates, target, index, ans, output);
+        int target = n;
+        int[] candidates = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int count = 0;
+        solve(candidates, target, index, ans, output, count, k);
         return ans;
     }
 }
